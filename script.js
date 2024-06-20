3a26edb0a8d2b2c071e923c64d035f1bcbf15532
bwondessen <bwondessen23@gmail.com>
2024-06-19 19:57:36 -0500

    Return random option from options array

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
 
+    const option = Math.floor(Math.random() * 3)
+    return options[option];
 }
 console.log(getRandomComputerResult());
