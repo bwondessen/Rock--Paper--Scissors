bde118dec4579f173350c5aa1f1a7a46796e6b46
bwondessen <bwondessen23@gmail.com>
2024-06-20 23:25:47 -0500

    Set up hasPlayerWonTheRound function

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-
-    const option = Math.floor(Math.random() * 3)
-    return options[option];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
 }
-console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
