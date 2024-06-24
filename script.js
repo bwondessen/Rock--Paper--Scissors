42bbbb7333430a29fa682a1e4a31fb7d65df881b
bwondessen <bwondessen23@gmail.com>
2024-06-23 23:49:32 -0500

    Determine wether player has won or lost round

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ function getRandomComputerResult() {
 }
 
 function hasPlayerWonTheRound(player, computer) {
-
+    if (player == "Rock" && computer == "Scissors") {
+        return true;
+    } else if (player == "Scissors" && computer == "Paper") {
+        return true;
+    } else if (player == "Paper" && computer == "Rock") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(hasPlayerWonTheRound("Rock", "Scissors"));
